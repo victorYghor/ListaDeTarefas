e58df339c23b1dbea5c4a0ef66fa011ff23f24e0
victor <devictoryghor@gmail.com>
2023-07-05 13:16:19 -0300

    try fixed the priority drop down menu

--- app/src/main/java/com/example/todo/components/PriorityDropDown.kt
+++ app/src/main/java/com/example/todo/components/PriorityDropDown.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -40,56 +42,66 @@ fun PriorityDropDown(
     priority: Priority,
     onPrioritySelected: (Priority) -> Unit,
     modifier: Modifier
-    ) {
+) {
     var expanded by remember { mutableStateOf(false) }
     val angle by animateFloatAsState(targetValue = if (expanded) 180f else 0f)
 
-    Row(modifier = modifier
-        .fillMaxWidth()
-        .height(TOP_APP_BAR_HEIGHT)
-        .clickable(onClick = { expanded = true })
-        .border(
-            width = 1.dp,
-            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0f),
-            shape = MaterialTheme.shapes.small
-        )
-        .background(topAppBarColor),
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT)
+            .clickable(onClick = { expanded = true })
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0f),
+                shape = MaterialTheme.shapes.small
+            )
+            .background(topAppBarColor),
         verticalAlignment = Alignment.CenterVertically
 
     ) {
-        Canvas(modifier = Modifier
-            .size(P_PRIORITY)
-            .weight(1f)) {
+        Canvas(
+            modifier = Modifier
+                .size(P_PRIORITY)
+                .weight(1f)
+        ) {
             drawCircle(color = priority.color)
         }
 
         Text(text = priority.name, color = textColor, modifier = Modifier.weight(8f))
-        
-        IconButton(onClick = { expanded = true }, modifier = Modifier
-            .weight(1.5f)
-            .alpha(5f)
-            .rotate(angle)) {
+
+        IconButton(
+            onClick = { expanded = true }, modifier = Modifier
+                .weight(1.5f)
+                .alpha(5f)
+                .rotate(angle)
+        ) {
             Icon(
                 imageVector = Icons.Filled.ArrowDropDown,
                 contentDescription = stringResource(R.string.drop_down_menu_icon)
             )
         }
         Priority.values().forEach {
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxWidth(fraction = 0.90f)
             ) {
-                DropdownMenuItem(
-                    text = { PriorityItem(priority = it) },
-                    onClick = {
-                        expanded = false
-                        onPrioritySelected(it)
-                    }
-                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier.fillMaxWidth(fraction = 0.90f)
+                ) {
+                    DropdownMenuItem(
+                        text = { PriorityItem(priority = it) },
+                        onClick = {
+                            expanded = false
+                            onPrioritySelected(it)
+                        }
+                    )
+                }
             }
         }
-
     }
 
 }
